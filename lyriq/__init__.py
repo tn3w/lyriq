c0c3e8e5185fa849937af4e7ffdcd5948f56a5b5
tn3w <tn3w@protonmail.com>
2025-07-15 01:37:59 +0200

    Update version to 1.1.3 and enhance project metadata
    - Bumped project version from 1.1.2 to 1.1.3 in __init__.py and pyproject.toml.
    - Updated development status classifier to reflect production readiness.
    - Corrected Documentation URL to point to the master branch.
    - Removed outdated Changelog URL from project metadata.

--- lyriq/__init__.py
+++ lyriq/__init__.py
@@ -10,7 +10,7 @@ Basic usage:
     >>> print(lyrics.plain_lyrics)
 """
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 __author__ = "TN3W"
 __license__ = "Apache-2.0"
 __email__ = "tn3w@protonmail.com"
